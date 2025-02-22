e51b7eb1ac04a4662adeef551a862ebf16bfaeae
geonwoo0215 <gw980215@naver.com>
2025-02-22 19:14:46 +0900

    initial commit

---
+++ src/main/java/com/pumping/PumpingApplication.java
@@ -0,0 +1,13 @@
+package com.pumping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PumpingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PumpingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pumping/PumpingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pumping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PumpingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

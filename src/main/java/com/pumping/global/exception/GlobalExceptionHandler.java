a89f4079f0ab24ffd81e358115982c8184712de3
geonwoo0215 <gw980215@naver.com>
2025-04-18 00:20:07 +0900

    refactor : 에러 로그 출력

--- src/main/java/com/pumping/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/pumping/global/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.pumping.global.exception;
 
 import com.pumping.domain.emailverification.exception.CodeVerificationException;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -8,24 +10,35 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.io.UncheckedIOException;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(UncheckedIOException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleFileProcessingException(UncheckedIOException e) {
+        log.error("{}",e.getMessage());
         return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler(CodeVerificationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleEmailVerificationException(CodeVerificationException e) {
+        log.error("{}",e.getMessage());
         return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler(RuntimeException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handlePasswordEncodingException(RuntimeException e) {
+        log.error("{}",e.getMessage());
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleEntityNotFoundException(EntityNotFoundException e) {
+        log.error("{}",e.getMessage());
         return new ErrorResponse(e.getMessage());
     }
 }
